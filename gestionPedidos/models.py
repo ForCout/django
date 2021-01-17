84439be029b72ab396c2ca57970a08e613e04ed1
forcout <monchoforcout@gmail.com>
2021-01-17 12:51:54 +0100

    jjjj

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -1,24 +1,20 @@
 from django.db import models
 
-class Clientes (models.Model):
-    nombre=models.CharField(max_length=50)
-    direccion=models.CharField(max_length=50)
-    email=models.CharField(max_length=50)
-    telefono=models.CharField(max_length=9)
 
-   
-        
+class Clientes (models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=9)
+
 
 class Articulos (models.Model):
-    nombre=models.CharField(max_length=45)
-    seccion=models.CharField(max_length=45)
-    precio=models.IntegerField()
+    nombre = models.CharField(max_length=45)
+    seccion = models.CharField(max_length=45)
+    precio = models.IntegerField()
 
-    
 
 class Pedidos (models.Model):
-    numero=models.IntegerField()
-    fecha=models.DateField()
-    entregado=models.BooleanField()
-
-    
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
